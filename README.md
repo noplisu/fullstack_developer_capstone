dd9f9790b39bccb3d892db58e36e2e5bd3aba90a
Grzegorz Lisowski <pros91@gmail.com>
2025-04-11 20:07:30 -0400

    Contenerize and add deployment

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - docker-compose up
 
 ## Start sentiment analyzer microservice
+- cd server/djangoapp/microservices
 - docker build . -t us.icr.io/${SN_ICR_NAMESPACE}/senti_analyzer
 - docker push us.icr.io/${SN_ICR_NAMESPACE}/senti_analyzer
 - ibmcloud ce application create --name sentianalyzer --image us.icr.io/${SN_ICR_NAMESPACE}/senti_analyzer --registry-secret icr-secret --port 5000
--- server/database/app.js
+++ server/database/app.js
@@ -8,8 +8,8 @@ const port = 3030;
 app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-const [reviews] = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
-const [dealerships] = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
+const { reviews } = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
+const { dealerships } = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
@@ -17,7 +17,6 @@ mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 const Reviews = require('./review');
 
 const Dealerships = require('./dealership');
-const dealership = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
@@ -89,25 +88,25 @@ app.get('/fetchDealer/:id', async (req, res) => {
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
-  [
+  ({
     name,
-    dealership_data=dealership,
-    review_data=review,
+    dealership,
+    review,
     purchase,
     purchase_date,
     car_make,
     car_model,
     car_year
-  ] = JSON.parse(req.body);
+  } = JSON.parse(req.body));
   const documents = await Reviews.find().sort( { id: -1 } );
-  let [id] = documents[0];
+  let { id } = documents[0];
   let new_id = id+1;
 
   const reviewModel = new Reviews({
 		id: new_id,
 		name,
-		dealership: dealership_data,
-		review: review_data,
+		dealership,
+		review,
 		purchase,
 		purchase_date,
 		car_make,
